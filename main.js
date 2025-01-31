4591bd149bbfd49b43445abc94a9de2e50e3518a
Talaat <talaatgamal.dev@gmail.com>
2025-01-31 18:52:48 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+  
+              
+// sidabar 
+              
+
+
+const menuIcon = document.querySelector('.sidebar-menu i');
+const sidebar = document.querySelector('.sidebar-content');
+
+menuIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
+
+
+
